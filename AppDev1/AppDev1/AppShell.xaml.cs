9e8b1472da16330dfff563d48c9657d3bfd4f6cb
Gabriel Fernandez <gbrlsfernandez@gmail.com>
2021-04-17 01:31:38 +0800

    herro master. testing the push to alipin branch

---
+++ AppDev1/AppDev1/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppDev1.ViewModels;
+using AppDev1.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppDev1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ AppDev1/AppDev1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDev1.ViewModels
+{
+    class HomeViewModel: BaseViewModel
+    {
+        public HomeViewModel()
+        {
+            Title = "Home";
+        }
+    }
+}
