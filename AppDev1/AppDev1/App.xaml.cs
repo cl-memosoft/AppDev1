2049e4888d02bbd8aa5047fb7eef1f6fbb1887ef
Coleen Ligasan <coleen_ligasan@memosoft.jp>
2021-04-16 22:18:08 +0800

    Added HomePage design. Installed sqlite pcl on ansroid.

---
+++ AppDev1/AppDev1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using AppDev1.Services;
+using AppDev1.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDev1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+
+            MainPage = new HomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
